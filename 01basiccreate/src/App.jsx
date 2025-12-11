ccc0f19a3a4283988cbd133df1732a2e0d89467a
Pundalik1998 <pundalikmasurkar2001@gmail.com>
2025-12-11 10:17:28 +0530

    bgchanger

--- 01basiccreate/src/App.jsx
+++ 01basiccreate/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h2 className="text-3xl font-bold underline">hello pundalik</h2>
+      
     </>
   )
 }
